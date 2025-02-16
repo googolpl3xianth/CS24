6798fbff9a4221115f46347c9dbea5760a7083f7
googolpl3xianth <68044994+googolpl3xianth@users.noreply.github.com>
2025-02-15 16:22:55 -0800

    Fixed some functions in person.cpp and changed setUnion to merge

--- labs/genepool/Person.cpp
+++ labs/genepool/Person.cpp
@@ -1,11 +1,5 @@
 #include "Person.h"
 
-void setUnion(std::set<Person*> &base, const std::set<Person*> &add){
-    for(Person* person : add){
-        base.insert(person);
-    }
-}
-
 // Person Member Functions
 
 Person::Person(){
@@ -39,21 +33,21 @@ std::set<Person*> Person::ancestors(PMod pmod){
     std::set<Person*> ancestors;
     if(mMother != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){
         ancestors.insert(mMother);
-        setUnion(ancestors, mMother->ancestors());
+        ancestors.merge(mMother->ancestors());
     }
     if(mFather != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){
         ancestors.insert(mFather);
-        setUnion(ancestors, mFather->ancestors());
+        ancestors.merge(mFather->ancestors());
     }
     return ancestors;
 }
 std::set<Person*> Person::aunts(PMod pmod, SMod smod){
     std::set<Person*> aunts;
     if(mMother != NULL && (pmod == PMod::ANY || pmod == PMod::MATERNAL)){
-        setUnion(aunts, mMother->sisters(PMod::ANY, smod));
+        aunts.merge(mMother->sisters(PMod::ANY, smod));
     }
     if(mFather != NULL && (pmod == PMod::ANY || pmod == PMod::PATERNAL)){
-        setUnion(aunts, mFather->sisters(PMod::ANY, smod));
+        aunts.merge(mFather->sisters(PMod::ANY, smod));
     }
     return aunts;
 }
@@ -79,10 +73,10 @@ std::set<Person*> Person::children(){ return mChildren; }
 std::set<Person*> Person::cousins(PMod pmod, SMod smod){
     std::set<Person*> cousins;
     for(Person* aunt : aunts(pmod, smod)){
-        setUnion(cousins, aunt->mChildren);
+        cousins.merge(aunt->children());
     }
     for(Person* uncle : uncles(pmod, smod)){
-        setUnion(cousins, uncle->mChildren);
+        cousins.merge(uncle->children());
     }
     return cousins;
 }
@@ -99,7 +93,7 @@ std::set<Person*> Person::descendants(){
     std::set<Person*> descendants = mChildren;
     if(!mChildren.empty()){
         for(Person* child : mChildren){
-            setUnion(descendants, child->descendants());
+            descendants.merge(child->descendants());
         }
     }
     return descendants;
@@ -107,7 +101,7 @@ std::set<Person*> Person::descendants(){
 std::set<Person*> Person::grandchildren(){
     std::set<Person*> grandchildren;
     for(Person* child : mChildren){
-        setUnion(grandchildren, child->mChildren);
+        grandchildren.merge(child->children());
     }
     return grandchildren;
 }
@@ -123,7 +117,7 @@ std::set<Person*> Person::granddaughters(){
 std::set<Person*> Person::grandfathers(PMod pmod){
     std::set<Person*> grandfathers;
     if(mFather == NULL){ return grandfathers; }
-    if(mFather->mMother != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){ grandfathers.insert(mFather->mMother); }
+    if(mMother->mFather != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){ grandfathers.insert(mMother->mFather); }
     if(mFather->mFather != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){ grandfathers.insert(mFather->mFather); }
     return grandfathers;
 }
@@ -131,13 +125,13 @@ std::set<Person*> Person::grandmothers(PMod pmod){
     std::set<Person*> grandmothers;
     if(mMother == NULL){ return grandmothers; }
     if(mMother->mMother != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){ grandmothers.insert(mMother->mMother); }
-    if(mMother->mFather != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){ grandmothers.insert(mMother->mFather); }
+    if(mFather->mMother != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){ grandmothers.insert(mFather->mMother); }
     return grandmothers;
 }
 std::set<Person*> Person::grandparents(PMod pmod){
     std::set<Person*> grandparents;
-    if(mMother != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){ setUnion(grandparents, mMother->parents()); }
-    if(mFather != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){ setUnion(grandparents, mFather->parents()); }
+    if(mMother != NULL && (pmod == PMod::MATERNAL || pmod == PMod::ANY)){ grandparents.merge(mMother->parents()); }
+    if(mFather != NULL && (pmod == PMod::PATERNAL || pmod == PMod::ANY)){ grandparents.merge(mFather->parents()); }
     return grandparents;
 }
 std::set<Person*> Person::grandsons(){
@@ -152,21 +146,21 @@ std::set<Person*> Person::grandsons(){
 std::set<Person*> Person::nephews(PMod pmod, SMod smod){
     std::set<Person*> nephews;
     for(Person* sibling : siblings(pmod, smod)){
-        setUnion(nephews, sibling->sons());
+        nephews.merge(sibling->sons());
     }
     return nephews;
 }
 std::set<Person*> Person::nieces(PMod pmod, SMod smod){
     std::set<Person*> nieces;
     for(Person* sibling : siblings(pmod, smod)){
-        setUnion(nieces, sibling->daughters());
+        nieces.merge(sibling->daughters());
     }
     return nieces;
 }
 std::set<Person*> Person::parents(PMod pmod){
     std::set<Person*> parents;
-    if(mMother != NULL){ parents.insert(mMother); }
-    if(mFather != NULL){ parents.insert(mFather); }
+    if(mMother != NULL && (pmod == PMod::ANY || pmod == PMod::MATERNAL)){ parents.insert(mMother); }
+    if(mFather != NULL && (pmod == PMod::ANY || pmod == PMod::PATERNAL)){ parents.insert(mFather); }
     return parents;
 }
 std::set<Person*> Person::siblings(PMod pmod, SMod smod){
@@ -217,10 +211,10 @@ std::set<Person*> Person::sons(){
 std::set<Person*> Person::uncles(PMod pmod, SMod smod){
     std::set<Person*> uncles;
     if(mMother != NULL && (pmod == PMod::ANY || pmod == PMod::MATERNAL)){
-        setUnion(uncles, mMother->brothers(PMod::ANY, smod));
+        uncles.merge(mMother->brothers(PMod::ANY, smod));
     }
     if(mFather != NULL && (pmod == PMod::ANY || pmod == PMod::PATERNAL)){
-        setUnion(uncles, mFather->brothers(PMod::ANY, smod));
+        uncles.merge(mFather->brothers(PMod::ANY, smod));
     }
     return uncles;
 }   
